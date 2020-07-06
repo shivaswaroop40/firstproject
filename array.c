9eb238b953e5976f5724d2737f3235c080f8fa61
shivaswaroop40 <67726666+shivaswaroop40@users.noreply.github.com>
2020-07-06 11:29:23 +0530

    Created array.c
    A C program to accept elements for a 3x3 array print it out in matrix form and calculate the sum diagonal elements

---
+++ array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main(){
+	int i=0,j=0,sum=0;
+	int mat[3][3];
+	int *a;
+//	a=&mat
+	printf("Enter the values of matrix: ");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			scanf("%d", &mat[i][j]);
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			if(i==j){
+				sum = sum + mat[i][j];
+			}	
+		}
+	}
+	printf("\nThe matrix values are\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nThe sum of diagonal elements is %d", sum);
+}
